f3fce7b1c6ba22dcd54199b73c89a07c227b9515
Hiếu Nguyễn Quốc <hieunguyenquoc1999@gmail.com>
2023-07-14 10:10:22 +0700

    Upload file

--- src/get_result.py
+++ src/get_result.py
@@ -1,5 +1,6 @@
 from src.extract_content_event import Extract_content
 from src.extraction_title_event import Extract_title
+from underthesea import sent_tokenize
 
 class Get_result:
     '''Khai báo các biến cần thiết'''
@@ -8,38 +9,39 @@ class Get_result:
         self.find_content = Extract_content()
     
     '''Tạo kết quả'''
-    def get_result(self, public_date, content):
+    def get_result(self, title, text, public_date):
         #Bài báo
-        content_lst = content.strip().split(".")
+        # content_final = title + "." + " " + text if not ". " in title else title + text
+        content_lst = sent_tokenize(text.strip())
 
         # #Danh sách các tiêu đề
         # lst_title = self.find_title_and_object.find_title(content_lst)
         
         # danh sách kết quả
         result = []
-        content_to_predict = ""
-        if content_lst[0] == "" :
-            content_to_predict = content_lst[1]
-        else:
-            content_to_predict = content_lst[0]
+        # content_to_predict = ""
+        # if content_lst[0] == "" :
+        #     content_to_predict = content_lst[1]
+        # else:
+        #     content_to_predict = content_lst[0]
 
         # for i in range(len(lst_title)):
-        index_of_title = content_lst.index(content_to_predict)
+        # index_of_title = content_lst.index(title)
 
         #tìm kiếm nội dung của sự kiện
-        content = " ".join(self.find_content.extract_content(content_to_predict, content_lst[index_of_title+1:index_of_title+11]))
+        content = " ".join(self.find_content.extract_content(title, content_lst[0:11]))
 
         #tìm kiếm các chủ thế, khách thể của sự kiện
-        chu_the, khach_the = self.find_title_and_object.find_oject(content_to_predict)
+        chu_the, khach_the = self.find_title_and_object.find_oject(title)
 
         #tìm kiếm thời gian của sự kiện
-        thoi_gian = self.find_title_and_object.find_time(content_to_predict, public_date, content)
+        thoi_gian = self.find_title_and_object.find_time(title, public_date, content)
 
         #tìm kiếm địa điểm của sự kiện
-        location = self.find_title_and_object.find_location(content_to_predict,content)
+        location = self.find_title_and_object.find_location(title,content)
         result_dict = {
                 # 'Sự kiện' : i + 1,
-                'Tiêu đề' : content_to_predict,
+                'Tiêu đề' : title,
                 'Chủ thể' : chu_the,
                 'Khách thể' : khach_the,
                 'Thời gian' : thoi_gian,
